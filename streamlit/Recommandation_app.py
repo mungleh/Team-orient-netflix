6d25e5d6894763c218c826b6e9a43f45fba73b8e
maxime lu <merdeth69@gmail.com>
2023-10-25 15:25:29 +0200

    weee

--- streamlit/Recommandation_app.py
+++ streamlit/Recommandation_app.py
@@ -1,6 +1,8 @@
 import os
+import json
 import requests
 import pandas as pd
+import mysql.connector
 import streamlit as st
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -14,6 +16,15 @@ host = os.getenv("host")
 port = os.getenv("port")
 dbname = os.getenv("dbname")
 
+mydb = mysql.connector.connect(
+  host=host,
+  user=username,
+  password=password,
+  database=dbname
+)
+
+mycursor = mydb.cursor()
+
 connection_string = f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{dbname}"
 engine = create_engine(connection_string)
 
@@ -22,8 +33,10 @@ api_port = os.getenv("api_port")
 
 ngrok = f"{api_ip}:{api_port}"
 
+
 # requests
 df = pd.read_sql_query("SELECT * FROM movie_table", engine)
+
 
 # app
 st.set_page_config(
@@ -35,13 +48,33 @@ option = st.selectbox('What film de you like ?', df)
 
 st.write(f"You chose {option}")
 
+
 if st.button("Give me recomendations !"):
 
-    response = requests.post(f"http://{ngrok}/predict?input={option}")
+    response = requests.post(f"http://{ngrok}/predict?input_title={option}")
 
     # Check the response status code
     if response.status_code == 200:
         # Print the response content
-        st.write(response.content)
+
+        recomendations = response.json()
+
+        st.json(recomendations)
+
+        recomendations_dict = json.loads(recomendations)
+
+        formatted_json = json.dumps(recomendations_dict, indent=4)
+
+        # put = pd.read_sql_query(f"INSERT INTO `inputs-table` (input, output) VALUES ('{option}', '[{formatted_json}]')", engine)
+
+        sql = "INSERT INTO `inputs-table` (input, output) VALUES (%s, %s)"
+        val = (f'{option}', '[{formatted_json}]')
+        mycursor.execute(sql, val)
+
+        # with engine.connect() as conn:
+        #     conn.execute(put)
+
+        # st.write(f"INSERT INTO `inputs-table` (input, output) VALUES ('{option}', [{formatted_json}])")
+
     else:
         st.write(f"Error - Status code: {response.status_code}")
