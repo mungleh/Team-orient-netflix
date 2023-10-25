2abdd726ce0a11f3b47448647b29712c8c4ab539
eliana cuhadar <eliana.cuhadar.69@hotmail.com>
2023-10-25 14:30:37 +0200

    :wrench: changing API output to JSON

--- api/api.py
+++ api/api.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import MinMaxScaler
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import pandas as pd
+from urllib.parse import quote
 import os
 
 app = FastAPI()
@@ -29,29 +30,33 @@ ngrok = f"{api_ip}:{api_port}"
 # requests
 df = pd.read_sql_query("SELECT * FROM input_table", engine)
 
+
 @app.post("/predict")
 def get_recommendations(input_title):
+
+
     input_movie = df[df['movie_title'] == input_title]
-    
-    features = input_movie[['actor_1_name', 'genres', 'imdb_score', 'director_name', 'duration', 'language', 'country']]
-    
+
     tfidf = TfidfVectorizer()
     tfidf_matrix = tfidf.fit_transform(df['genres'] + " " + df['actor_1_name'] + " " + df['director_name'] + " " + df['language'] + " " + df['country'])
-    
+
     scaler = MinMaxScaler()
     numerical_features = scaler.fit_transform(df[['imdb_score', 'duration']])
-    
-    combined_features = pd.concat([pd.DataFrame(tfidf_matrix.toarray()), pd.DataFrame(numerical_features)], axis=1)
-    
-    cosine_sim = cosine_similarity(combined_features, combined_features)
-    
-    idx = df.index[df['movie_title'] == input_title].tolist()[0]
-    
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    
-    sim_indices = [i[0] for i in sim_scores[1:6]]  
-    
-    return df['movie_title'].iloc[sim_indices]
 
+    combined_features = pd.concat([pd.DataFrame(tfidf_matrix.toarray()), pd.DataFrame(numerical_features)], axis=1)
+
+    cosine_sim = cosine_similarity(combined_features, combined_features)
+
+    idx = df.index[df['movie_title'] == input_title].tolist()[0]
+
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    sim_indices = [i[0] for i in sim_scores[1:6]]
+
+    output = df['movie_title'].iloc[sim_indices]
+
+    json_output = output.to_json()
+
+    return json_output
