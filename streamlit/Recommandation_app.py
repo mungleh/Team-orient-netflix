70c3a9cb8da6dd4578376af9277b37189e348a72
maxime lu <merdeth69@gmail.com>
2023-10-27 15:14:45 +0200

    letsgo

--- streamlit/Recommandation_app.py
+++ streamlit/Recommandation_app.py
@@ -1,8 +1,9 @@
 import os
-import json
+import base64
 import requests
 import pandas as pd
 import streamlit as st
+import streamlit_nested_layout
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from urllib.parse import quote
@@ -29,16 +30,39 @@ ngrok = f"{api_ip}:{api_port}"
 df = pd.read_sql_query("SELECT * FROM movie_table", engine)
 
 
-# app
-st.set_page_config(
-    layout="wide"
-)
+# # app
+# st.set_page_config(
+#     layout="wide"
+# )
 
 
-option = st.selectbox('What film de you like ?', df)
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
 
-st.write(f"You chose {option}")
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('assets/walle.jpg')
+
+st.markdown(f"<h1 style='color: white;'>What film de you like ?</h1>", unsafe_allow_html=True)
+option = st.selectbox('', df)
+
+
+st.markdown(f"<h6 style='color: white;'>You chose {option}</h6>", unsafe_allow_html=True)
+
+st.markdown('#')
 
 if 'recommendations_movies' not in st.session_state:
     st.session_state.recommendations_movies = []
@@ -59,25 +83,40 @@ if st.button("Give me recomendations !"):
 
         recommendations_movies = ', '.join(recommendations)
 
-        st.write(recommendations_movies)
-
     else:
         st.write(f"Error - Status code: {response.status_code}")
 
+st.markdown('#')
+
 for idx, movie in enumerate(st.session_state.recommendations_movies, 1):
     checkbox_value = movie in st.session_state.selected_movies
-    checked = st.checkbox(f"{idx}. {movie}", value=checkbox_value)
+    # checked = st.checkbox(f"{idx}. {movie}", value=checkbox_value)
+
+    checkbox_label = f"<h6 style='color: white;'>{idx}. {movie}</h6>"
+
+    col1, col2 = st.columns(2, gap="small")
+
+    with col2:
+        checked = st.checkbox(label="", key=idx)
+
+    with col1:
+        st.markdown(checkbox_label, unsafe_allow_html=True)
+
     if checked and movie not in st.session_state.selected_movies:
         st.session_state.selected_movies.append(movie)
     elif not checked and movie in st.session_state.selected_movies:
         st.session_state.selected_movies.remove(movie)
 
-st.write(f"You selected: {st.session_state.selected_movies}")
+st.markdown('#')
+
+st.markdown(f"<h6 style='color: white;'>You selected: {st.session_state.selected_movies}</h6>", unsafe_allow_html=True)
+
+st.markdown('#')
 
 if st.button("OK"):
     if st.session_state.selected_movies:
         selected_movies_str = ', '.join(st.session_state.selected_movies)
-        st.write(f"You selected: {selected_movies_str}")
+        st.markdown(f"<h6 style='color: white;'>You selected: {selected_movies_str}</h6>", unsafe_allow_html=True)
         recommendations_movies = ', '.join(st.session_state.recommendations_movies)
     else:
         selected_movies_str = 'no selections'
@@ -99,6 +138,6 @@ if st.button("OK"):
     st.session_state.selected_movies = []
     st.session_state.recommendations_movies = []
 
-    st.write("Preferences saved to database!")
+    st.markdown(f"<h6 style='color: white;'>Preferences saved to database!</h6>", unsafe_allow_html=True)
 else:
-    st.write("You haven't selected any movies.")
+    st.markdown(f"<h6 style='color: white;'>You haven't selected any movies.</h6>", unsafe_allow_html=True)
--- streamlit/pages/Data_visualisation.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-import pandas as pd
-import streamlit as st
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
-
-st.title("Film statistics")
